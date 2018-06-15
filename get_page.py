166ab96c0339384c9ffe6fec745fd48351ef1d30
Green Wang <cocowool@qq.com>
2018-06-15 16:57:23 +0800

    modify parameter

--- get_page.py
+++ get_page.py
@@ -8,14 +8,24 @@ import requests
 
 # 使用 urllib 方式获取
 response = urllib.request.urlopen('http://www.baidu.com')
+# 打印服务器返回的状态码
+print(response.code)
 # read() 读取的是服务器的原始返回数据 decode() 后会进行转码
 print(response.read().decode())
 
 # 使用 requests 方式获取
 # request 模块相比
 resp = requests.get('http://www.baidu.com')
+# 打印服务器返回的状态码
+print(resp.status_code)
 print(resp)
 print(resp.text)
+
+# HTTP 是基于请求和响应的工作模式，urllib.request 提供了一个 Request 对象来代表请求，因此上面的代码也可以这么写
+req = urllib.request.Request('http://www.baidu.com')
+with urllib.request.urlopen(req) as response:
+    print(response.read())
+
 
 # 使用带参数的接口访问
 tencent_api = "http://qt.gtimg.cn/q=sh601939"
@@ -27,3 +37,5 @@ print(response.read())
 resp = requests.get(tencent_api)
 print(resp)
 print(resp.text)
+
+
