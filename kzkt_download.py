7cb0a942d75056b20ea60de88dee1b72c9a98a99
Shiqiang <cocowool@qq.com>
2020-11-24 12:07:51 +0800

    modify code

--- kzkt_download.py
+++ kzkt_download.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# 抓取空中课堂学习资料，整理并保存
+# 抓取空中课堂学习资料，整理并保存。
 
 # https://cache.bdschool.cn/public/bdschool/index/static/migu/w.html?grade=3
 
@@ -28,12 +28,30 @@ class kzkt():
 
     # 遍历需要下载的视频和视频标题
     def parse_video(self, html):
+        # print(type(html))
         soup = BeautifulSoup(html, 'html.parser')
-        work_table = soup.find('span', attrs={'class':'content_table'}).contents
-        print(work_table)
+        work_table = soup.find_all('table', attrs={'class':'content_table', 'grade' : '3'})
+        print(len(work_table))
+        pass
+
+    # 下载页面中的视频及课件
+    def download_video(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # print(response.text)
+        # print(soup.find_all('video'))
+        print(soup.find_all('a', attrs={'class':'file_view_list'}))
+
+        file_name = '1123.ppt'
+        ppt_url = "https://cache.bdschool.cn/index.php?app=interface&mod=Resource&act=download&id=872462"
+
         pass
 
 # 实例化
-url = "https://cache.bdschool.cn/public/bdschool/index/static/migu/w.html?grade=3&_d=2020/11/23"
+course_url = "https://cache.bdschool.cn/public/bdschool/index/static/migu/w.html?grade=3&_d=2020/11/23"
+video_url = "https://cache.bdschool.cn/public/bdschool/index/static/migu/weike/0bf79a3ba787b32a7ed2df8844e7dd8e.html?grade_id=3&subject_id=1"
 dog = kzkt()
-dog.parse_video(dog.get_html)
+# dog.parse_video(dog.get_html(url))
+
+dog.download_video(video_url)
