679824f525f22da161ed273c2f25bc2360ede950
Shiqiang <cocowool@qq.com>
2021-02-09 19:06:50 +0800

    modify code

---
+++ get_shici.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 收集中华古诗词
+# 本脚本仅用于个人学习
+
+import os, sys, getopt
+import re
+from bs4 import BeautifulSoup
+import requests
+import time
+import cgi
+
+# https://www.shici123.cn/song/-------1
+
+class SCSpider():
+    pass
+
