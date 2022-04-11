26b9f134b8a8678c7e1b8499cae5273c8a17f2d4
Shiqiang <cocowool@qq.com>
2022-04-11 22:43:12 +0800

    modify spider daemon

--- poem-spider/ps_daemon.py
+++ poem-spider/ps_daemon.py
@@ -11,6 +11,12 @@ import signal
 # 每 x 分钟从配置文件扫描需要抓取的网站
 # 每 x 分钟记录当前抓取的进展
 
+class DeamonDecorator:
+    pass
+
+class PoemSpiderDeamon(DeamonDecorator):
+    pass
+
 def ps_daemon(pid_file = None):
     # 从父进程 Fork 子进程出来
     pid = os.fork()
@@ -42,12 +48,25 @@ if __name__ == '__main__':
         print(help_msg)
         sys.exit(1)
 
-#     print("Start ps daemon")
-#     time.sleep(10)
-#     print("End ps daemon")
+    psd_name = 'poem_spider_daemon'
+    pid_file = '/Users/shiqiang/Projects/py-spider/poem-spider/psd.pid'
+    log_file = '/Users/shiqiang/Projects/py-spider/poem-spider/psd.log'
+    err_file = '/Users/shiqiang/Projects/py-spider/poem-spider/psd-error.log'
 
-# print("Main Process Start")
-# m = threading.Thread(target=ps_daemon, args=())
-# m.start()
-# time.sleep(1)
-ps_daemon()
+    psd = PoemSpiderDeamon(psd_name, pid_file, stderr=err_file, verbose=1)
+
+    if sys.argv[1] == 'start':
+        psd.start(log_file)
+    elif sys.argv[1] == 'stop':
+        psd.stop()
+    elif sys.argv[1] == 'restart':
+        psd.restart(log_file)
+    elif sys.argv[1] == 'status':
+        alive = psd.is_running()
+        if alive:
+            print("Poem Daemon [%s] is running ......" % (psd.get_pid()) )
+        else:
+            print("Poem Daemon stopped")
+    else:
+        print("Invalid parameter")
+        print(help_msg)
