62ae155fdfa9053c3ff99f8d8ebbd1f81580d88a
Shiqiang <cocowool@qq.com>
2022-04-11 21:43:34 +0800

    modify spider daemon

--- poem-spider/ps_daemon.py
+++ poem-spider/ps_daemon.py
@@ -3,7 +3,19 @@
 import os
 import sys
 import atexit
+import time
+import threading
 
 # Poem Spider Deamon
 # 每 x 分钟从配置文件扫描需要抓取的网站
 # 每 x 分钟记录当前抓取的进展
+
+def ps_daemon():
+    print("Start ps daemon")
+    time.sleep(10)
+    print("End ps daemon")
+
+print("Main Process Start")
+m = threading.Thread(target=ps_daemon, args=())
+m.start()
+time.sleep(1)
