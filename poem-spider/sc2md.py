1a5ac6fe3015ae70740e1365256818f153b48ec5
Shiqiang <cocowool@qq.com>
2022-06-20 23:13:33 +0800

    json to markdown tool

--- poem-spider/sc2md.py
+++ poem-spider/sc2md.py
@@ -8,12 +8,14 @@ import json
 def save_to_markdown(json_content, markdown_file):
     pass
 
-json_files = '/Users/shiqiang/Projects/shici123/json_files/'
-markdown_files = '/Users/shiqiang/Projects/shici123/markdown_files/'
-for parent, _, file_names in os.walk(json_files):
-    for file_name in file_names:
-        full_json_file = os.path.join(parent, file_name)
-        markdown_file = markdown_files + file_name
-        with open(full_json_file, 'r') as fp:
-            json_content = json.load(fp)
-            print(json_content)
+
+if __name__ == "__main__":
+    json_files = '/Users/shiqiang/Projects/shici123/json_files/'
+    markdown_files = '/Users/shiqiang/Projects/shici123/markdown_files/'
+    for parent, _, file_names in os.walk(json_files):
+        for file_name in file_names:
+            full_json_file = os.path.join(parent, file_name)
+            markdown_file = markdown_files + file_name
+            with open(full_json_file, 'r') as fp:
+                json_content = json.load(fp)
+                print(json_content)
