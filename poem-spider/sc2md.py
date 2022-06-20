7ce8b092a6c5a37df096cd42b4402fed26d55ce0
Shiqiang <cocowool@qq.com>
2022-06-20 23:06:56 +0800

    json to markdown tool

--- poem-spider/sc2md.py
+++ poem-spider/sc2md.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
+import json
 
 # Convert spider json files to markdown file
+
+# markdown file name pattern "唐-李白-静夜思"
+def save_to_markdown(json_content, markdown_file):
+    pass
 
 json_files = '/Users/shiqiang/Projects/shici123/json_files/'
 markdown_files = '/Users/shiqiang/Projects/shici123/markdown_files/'
 for parent, _, file_names in os.walk(json_files):
     for file_name in file_names:
-        print(file_name)
+        full_json_file = os.path.join(parent, file_name)
         markdown_file = markdown_files + file_name
+        with open(full_json_file, 'r') as fp:
+            json_content = json.load(fp)
+            print(json_content)
