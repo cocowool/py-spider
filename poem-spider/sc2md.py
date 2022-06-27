7f12d57a94975a2616fb72c83824b28c4eee299b
Shiqiang <cocowool@qq.com>
2022-06-27 20:05:39 +0800

    modify post

--- poem-spider/sc2md.py
+++ poem-spider/sc2md.py
@@ -1,12 +1,38 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+import time
 
 # Convert spider json files to markdown file
 
+def get_human_time():
+    fmt = '%Y-%m-%d %H-%M-%S'
+
+    ts = time.time()
+    ta = time.localtime(ts)
+    t = time.strftime(fmt, ta)
+    return t
+
 # markdown file name pattern "唐-李白-静夜思"
 def save_to_markdown(json_content, markdown_file):
-    pass
+    md_string = ''
+    hexo_date = get_human_time()
+    hexo_top = '''---
+title: {hexo_title}
+date: {hexo_date}
+tag: 
+---\n'''.format(hexo_title=json_content['p_title'], hexo_date=hexo_date)
+
+    md_string += hexo_top
+
+    md_string += '## ' + json_content['p_title'] + '\n'
+    md_string += '\n'
+    md_string += '> ' + json_content['p_author'] + ' \n'
+    md_string += '\n'
+    md_string += json_content['p_content']
+
+    print(json_content)
+    print(md_string)
 
 
 if __name__ == "__main__":
@@ -18,4 +44,4 @@ if __name__ == "__main__":
             markdown_file = markdown_files + file_name
             with open(full_json_file, 'r') as fp:
                 json_content = json.load(fp)
-                print(json_content)
+                save_to_markdown(json_content, '')
